2227421151a48312804c9111f0fe0c87bd819462
odedmo <odedmoran222@gmail.com>
2022-08-10 21:49:14 +0300

    add logo image

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,20 @@
-export default function About() {
+import Head from 'next/head'
+
+const About = () => {
   return (
-    <div>about</div>
-  )
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas"/>
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+      </div>
+    </>
+  );
 }
+ 
+export default About;
