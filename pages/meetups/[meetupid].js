9a08000e59abb937a859931d267e0cc26b5e395b
odedmo <odedmoran222@gmail.com>
2022-08-31 09:57:50 +0300

    add dynamic meetup page

---
+++ pages/meetups/[meetupid].js
@@ -0,0 +1,12 @@
+const MeetupDetails = () => {
+  return (
+    <>
+      <img src='https://picsum.photos/200/300' alt='meetup image'/>
+      <h1>Meetup</h1>
+      <address>address</address>
+      <p>description</p>
+    </>
+  )
+}
+
+export default MeetupDetails
